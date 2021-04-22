12f318b1076a6968a0388ce2b5d46ab0436aee7d
Mark Alldritt <alldritt@latenightsw.com>
2021-04-22 16:09:24 -0700

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,15 @@
+import PackageDescription
+
+let package = Package(
+    name: "TaggerKit",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(name: "TaggerKit", targets: ["TaggerKit"])
+    ],
+    targets: [
+        .target(
+            name: "TaggerKit",
+            path: "TaggerKit"
+        )
+    ]
+)
